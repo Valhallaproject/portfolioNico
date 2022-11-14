281b1c9c37571a9f9a15ad36334753aa0c508fdc
Nicolas Bictel <valhallaproject@outlook.fr>
2022-11-14 17:58:17 +0100

    modif fomr contact

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import emailjs from "emailjs-com";
 import { useRef } from "react";
 
 import "./contact.css";
 
+
+
 function contact() {
+  const [name, setName] = useState("")
+const [mail, setMail] = useState("")
+const [message, setMessage] = useState("")
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -20,6 +25,10 @@ function contact() {
       .then(
         (result) => {
           console.log(result.text);
+          alert("Votre message a été envoyé")
+          setMail("")
+          setName("")
+          setMessage("")
         },
         (error) => {
           console.log(error.text);
@@ -36,15 +45,15 @@ function contact() {
           <form ref={form} onSubmit={sendEmail} id="myForm">
             <div className="infoSend">
               <label>Nom :</label>
-              <input type="text" name="name" id="name" />
+              <input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)}/>
             </div>
             <div className="infoSend">
               <label>Email :</label>
-              <input type="email" name="email" id="email" />
+              <input type="email" name="email" id="email" value={mail} onChange={(e) => setMail(e.target.value)}/>
             </div>
             <div className="message">
               <label className="textarea">Message :</label>
-              <textarea name="message" id="message" />
+              <textarea name="message" id="message" value={message} onChange={(e) => setMessage(e.target.value)}/>
             </div>
             <input type="submit" value="Envoyer" className="button" />
           </form>
